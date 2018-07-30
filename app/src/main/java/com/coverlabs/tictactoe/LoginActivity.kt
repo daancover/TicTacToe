dd19d0c030f581381b222eaee48d9c8d9c81225a
daancover <daniel.salgado@zup.com.br>
2018-07-30 16:32:45 -0300

    Created LoginActivity

---
+++ app/src/main/java/com/coverlabs/tictactoe/LoginActivity.kt
@@ -0,0 +1,20 @@
+package com.coverlabs.tictactoe
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_login.*
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        btSignIn.setOnClickListener { _ ->
+            var intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+}
